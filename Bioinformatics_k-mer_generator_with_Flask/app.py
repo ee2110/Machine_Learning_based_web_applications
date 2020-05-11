bc1818ffa87122ae2024ba0ba6762cd69e263b89
EeYeoKeat <eeyeokeat@gmail.com>
2020-05-12 02:23:40 +0800

    insert generator

--- Bioinformatics_k-mer_generator_with_Flask/app.py
+++ Bioinformatics_k-mer_generator_with_Flask/app.py
@@ -6,17 +6,43 @@ from flask import Flask, request, render_template, send_from_directory
 #creating instance of the class
 app = Flask(__name__)
 
+file_id = "output.txt"
+
+def generate_kmers(start, end):
+	for record in fileA[start:end]:
+		#file_id = "Fflaviviridae/Output_kmers_All-" + str(start) + "-" +  str(end) + ".txt"
+		nr_sequence = record.seq
+		seq_len = len(nr_sequence)
+		kmer = 9
+		count = 0
+		temp = []
+		for seq in list(range(seq_len-(kmer-1))):
+			count += 1
+			my_kmer = (nr_sequence[seq:seq+kmer])
+			temp.append(str(my_kmer))
+		with open(file_id, 'a') as f:
+			f.writelines("%s\n" % kmer for kmer in temp)
+
+
+
+
 #to tell flask what url shoud trigger the function index()
 @app.route('/', methods = ['GET'])
 def index():
     return render_template('main.html')
 
 @app.route('/file-downloads', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
-        f = request.files['file']  
-        f.save(f.filename)  
-        return render_template('generate.html', name = f.filename) 
+def success():
+	fasta_file = request.files['fasta']
+	fileA = list(SeqIO.parse(fasta_file,"fasta"))
+	n = len(fileA)
+	pool = ProcessPoolExecutor(12)
+	futures = []
+	perCPUSize = math.ceil(n/12)
+	for i in range(0,12):
+		futures.append(pool.submit(generate_kmers, i * perCPUSize, (i+1) * perCPUSize))  
+
+	return render_template('generate.html') 
 
 
 if __name__ == '__main__':
